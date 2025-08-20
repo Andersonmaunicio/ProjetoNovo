389472fe09144e54f45e37280d9cae1af1ac36aa
Andersonmaunicio <andersonmaunicio@gmail.com>
2025-08-20 14:17:22 -0300

    Validações no GET e SET

--- Models/Pessoa.cs
+++ Models/Pessoa.cs
@@ -5,9 +5,28 @@ using System.Threading.Tasks;
 
 namespace ExemploExplorando.Models
 {
-    public class Pessoa //Classe
+    public class Pessoa // Classe
     {
-        public string Nome { get; set; } //Propriedade
+        private string _nome;
+        public string Nome
+        {
+            get
+            {
+                return _nome.ToUpper();
+            }
+
+            set
+            {
+                if (value == "") // Argumento que está recebendo o nome
+                {
+                    throw new ArgumentException("O nome não pode ser vazio"); // Exceção: Não permiti que o código continue ser executado
+                }
+
+                _nome = value;
+            }
+        } //Propriedade
+
+
         public int Idade { get; set; } //Propriedade
 
         public void Apresentar()
