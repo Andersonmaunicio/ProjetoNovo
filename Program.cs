3887df10790638b7cc37177eb664e5884d600aa9
Andersonmaunicio <andersonmaunicio@gmail.com>
2025-08-21 22:05:09 -0300

    Concatenação de valores

--- Program.cs
+++ Program.cs
@@ -1,16 +1,46 @@
 ﻿using ExemploExplorando.Models; // Importando o nameSpace
 
 
-Pessoa p1 = new Pessoa(nome: "Ameno", sobrenome: "Lima");
-Pessoa p2 = new Pessoa(nome: "Ronar", sobrenome: "Nascimento");
+string numero1 = "10"; //resultado Concatenação
+string numero2 = "20"; //resultado Concatenação
 
-Curso cursoDeingles = new Curso();
-cursoDeingles.Nome = "Inglês";
-cursoDeingles.Alunos = new List<Pessoa>();
 
-cursoDeingles.AdicionarAluno(p1);
-cursoDeingles.AdicionarAluno(p2);
-cursoDeingles.ListarAlunos();
+// int numero1 = "10"; // Resultado adição
+// int numero2 = "20"; // Resultado adição
+
+// string numero1 = "10"; //resultado Concatenação
+// // int numero2 = "20"; //resultado Concatenação
+
+string resultado = numero1 + numero2;
+
+Console.WriteLine(resultado);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Pessoa p1 = new Pessoa(nome: "Ameno", sobrenome: "Lima");
+// Pessoa p2 = new Pessoa(nome: "Ronar", sobrenome: "Nascimento");
+
+// Curso cursoDeingles = new Curso();
+// cursoDeingles.Nome = "Inglês";
+// cursoDeingles.Alunos = new List<Pessoa>();
+
+// cursoDeingles.AdicionarAluno(p1);
+// cursoDeingles.AdicionarAluno(p2);
+// cursoDeingles.ListarAlunos();
 
 
 
