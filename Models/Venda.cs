fb3618c94eccc272f432c3a9766be54c6b025435
Andersonmaunicio <andersonmaunicio@gmail.com>
2025-08-24 10:40:29 -0300

    Atributos

--- Models/Venda.cs
+++ Models/Venda.cs
@@ -1,13 +1,16 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace Models
 {
     public class Venda
     {
         public int Id { get; set; }
+        [JsonProperty("Nome_Produto")] //Atributo
         public string Produto { get; set; }
         public decimal Preco { get; set; }
         public DateTime DataVenda { get; set; }
